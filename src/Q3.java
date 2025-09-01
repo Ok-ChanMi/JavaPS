39faa1ede3070150404beba1c26194642550c1ac
Ok-ChanMi <chan04152@naver.com>
2025-09-01 17:54:14 +0900

    문제 3번 이상..

--- src/Q3.java
+++ src/Q3.java
@@ -5,7 +5,7 @@
 2. 배는 매일 9시 부터 21시 전까지(21시를 포함하지 않습니다) 10분 단위로 들어옵니다.
 3. 전체 대기 인원은 14,000,605명입니다. 우리는 14,000,606번째와 14,000607번째에 배를 타게 됩니다.
 앞사람이 아프거나, 대를 못하고 빠질 경우 대기인원이 줄어들 수도 있습니다. 라이캣과 자바독이 다를 배를 타야 할 경우에는 뒷배를 타야합니다.
-4. 1월은 1024일, 2월은 512일, 3월은 265일, 4월은 128일, 5월은 64일, 6월은 32일, 7월은 16일, 8월은 8일, 9월은 4일, 10월은 2일이며, 10월까지 밖에 없습니다.
+4. 1월은 1024일, 2월은 512일, 3월은 256일, 4월은 128일, 5월은 64일, 6월은 32일, 7월은 16일, 8월은 8일, 9월은 4일, 10월은 2일이며, 10월까지 밖에 없습니다.
 5. 시간의 개념은 동일합니다.(하루는 24시간, 1시간 60분, 1분 60초/ 현재날짜는 2020년 1월 1일입니다.)
 6. 배에 타는 순간 자바독이 화장실이 급하다하여 화장시에 갔으며, 현재 시간에 '분'만큼 배 출발이 늦어졌습니다.
 7. 배는 휴일도 동일하게 운항됩니다. 배는 천재지변에 영향을 받지 않습니다. 마법으로 날라다니거든요
@@ -23,9 +23,11 @@ int 대이인원 = 12000202
 2020년 1월 1000일 11시 0분 출발
 */
 
+/*import java.util.Calendar;
+
 public class Q3 {
     public static void main(String[] args) {
-        int waitPeople = 14000605;
+        int waitPeople = 12000202;
         solution(waitPeople);
     }
     public static void solution(int waitPeople){
@@ -33,7 +35,6 @@ public class Q3 {
 
         for(int i = 10; i > 0; i--){
             days += (int)Math.pow(2,i);
-            System.out.println(days);
         }
 
         int waitDay = waitPeople /1200;
@@ -54,5 +55,51 @@ public class Q3 {
         int[] startMinute = {25, 40, 55, 70, 85, 100};
         int boardingTime = lastPeople % 100 + 1;
         int startOnTheTime = 0;
+
+        for (int i = 0; i < startMinute.length; i++){
+            if(startMinute[i] > boardingTime){
+                startOnTheTime = i *10;
+                break;
+            }
+        }
+        Calendar toDay = Calendar.getInstance();
+        int minute = toDay.get(Calendar.MINUTE) + startOnTheTime;
+
+        if(minute >= 60) {
+            minute -= 60;
+            clock++;
+        }
+        System.out.println((year + 2020) + "년 " + month + "월 " + day +"일 " + clock +"시 " + minute + "분");
     }
 }
+*/
+
+public class Q3{
+    public static void main(String[] args) {
+        solution(6);
+    }
+    public static void solution(int n){
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                count++;
+            }
+        }
+
+        for(int i =0; i < n; i++){
+            count++;
+        }
+
+        for(int i = 0; i < n * 2; i++){
+            count++;
+        }
+
+        for (int i = 0; i < 5; i++){
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
+
