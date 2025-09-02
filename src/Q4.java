38d1547fa0626cdee7be0ba9e94d5ceefd1fafdf
Ok-ChanMi <chan04152@naver.com>
2025-09-02 18:47:44 +0900

    Q4 다시 시작

---
+++ src/Q4.java
@@ -0,0 +1,52 @@
+/*정수형*/
+public class Q4 {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 4;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(" ");
+
+        System.out.println("정수형 비교연산");
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(" ");
+
+
+        System.out.println("정수형 비트 연산");
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(~a);
+        System.out.println(a << 2);
+        System.out.println(a >> 1);
+        System.out.println(" ");
+
+        System.out.println("부독소수형 사칙연산과 모듈러 연산");
+        System.out.println(2.5 + 3.7);
+        System.out.println(7.9 - 4.2);
+        System.out.println(1.5 * 4.8);
+        System.out.println(10.0 / 3.2);
+        System.out.println(10.0 % 3.2);
+        System.out.println(" ");
+
+        double x = 0.5;
+        double y = 1.2;
+        double z = 2.0;
+        System.out.println("부동소수형 논리 연산");
+        System.out.println(x > y && y < z);
+        System.out.println(x < y || y < z);
+        System.out.println(!(x > y));
+
+
+    }
+
+}
